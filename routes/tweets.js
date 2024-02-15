c288072b28d2c6492ace3b290b70307574d3593e
Antoine <antoine.beppo@gmail.com>
2024-02-15 17:01:10 +0100

    route post tweet

--- routes/tweets.js
+++ routes/tweets.js
@@ -2,9 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 require('../models/connection');
-const Tweets = require('../models/tweets');
-const { checkBody } = require('../modules/checkBody');
+const Tweet = require('../models/tweets');
+const User = require('../models/users')
 
 
+router.post('/addtweet/:token', (req, res) => {
+    console.log("Admin addtweet") 
+    //    Utiliser un regExp pour extraire les trends 
+    let regExp = /\#\w*/g ;
+    console.log(req.body.text)
+    const trendOftweet = Array.from(req.body.text.matchAll(regExp));
+    // utiliser un findOne avec token pour le rattacher au tweet
+    User.findOne({ token: req.params.token }).then(data => {
+        console.log(data.token) 
+        const newTweet = new Tweet ({
+            date: Date.now(),
+            text: req.body.text,
+            trend: trendOftweet,
+            numberLikes: [],
+            author: data.id,
+          });
+          newTweet.save().then(newDoc => {
+            res.json({ result: true, trend: newDoc.trend })
+        });
+      })
+})
 
-module.exports = router;
+
+router.delete("/:tweets", (req, res) => {
+    Tweet.deleteOne({id})
+    res.json({ result: true });
+ })
+
+ module.exports = router;
